34a2bbe964b3e3aac952637d08342b5df881f9b2
alexstepa2001 <alexstepa2001@icloud.com>
2022-05-30 12:05:05 +0300

    fixed all problems with scss

---
+++ slogan-app/src/App.js
@@ -0,0 +1,12 @@
+import TestC from './components/TestC';
+import './styles/style.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <TestC/>
+    </div>
+  );
+}
+
+export default App;
---
+++ slogan-app/src/components/TestC.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class TestC extends Component {
+    render() {
+        return (
+            <div className = "GitTest">
+                <a href="">Очень</a>
+                <a href="">Странно</a>
+                <a href="">Тестирую</a>
+                <a href="">Git</a>
+            </div>
+        )
+    }
+}
