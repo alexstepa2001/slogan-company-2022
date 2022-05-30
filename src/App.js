3fdaa57d7c73b60f10b707466a2c4db15e28c5d9
alexstepa2001 <alexstepa2001@icloud.com>
2022-05-30 11:41:20 +0300

    first_use_commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Navigation from './Navigation'
+import './styles/main.scss'
+
+function App() {
+  return (
+    <Navigation/>
+  );
+}
+
+export default App;
---
+++ src/Navigation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class navigation extends Component {
+    render() {
+        return (
+            <nav className='main_nav'>
+                <a href="">Хехе 1</a>
+                <a href="">Хехе 2</a>
+                <a href="">Хехе 3</a>
+            </nav>
+        )
+    }
+}
