e777eeb0bc232d936c5f6320ad48c31579206ef3
alexstepa2001 <alexstepa2001@icloud.com>
2022-06-01 18:13:49 +0300

    Сверстал страницу 'Incoming claim'

--- slogan-app/src/Claims.js
+++ slogan-app/src/Claims.js
@@ -2,6 +2,7 @@ import Navigation from './components/Navigation'
 import Header from './components/Header';
 import ClaimsSection from './components/ClaimsSection';
 import NewClaimSection from './components/NewClaimSection';
+import IncomingClaimSection from './components/IncomingClaimSection';
 import './styles/style.scss';
 import './styles/claims.scss';
 import {Routes, Route, Link} from 'react-router-dom';
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path = '/' element = {<ClaimsSection/>}/>
         <Route path = '/new_claim' element = {<NewClaimSection/>}/>
+        <Route path = '/incoming_claim' element = {<IncomingClaimSection/>}/>
       </Routes>
     </div>
   </>
---
+++ slogan-app/src/components/IncomingClaimSection.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import '../styles/new_claims.scss';
+import {Link} from 'react-router-dom';
+
+export default class IncomingClaimSection extends Component {
+    render() {
+    return  (
+            <section className="create_new_claim_section incoming">
+                <h1>Incoming claim</h1>
+                <form action="">
+                    <p>Title</p>
+                    <input type="text" disabled placeholder = "Type claim title"/>
+                    <p>Type</p>
+                    <select name="" id="" disabled className= "new_claim_input" >
+                        <option value="" disabled selected>Select type</option>
+                        <option value="hardware">Hardware</option>
+                        <option value="software">Software</option>
+                        <option value="troubleshooting">Troubleshooting</option>
+                        <option value="networking">Networking</option>
+                    </select>
+                    <p>Description</p>
+                    <input type="text" disabled placeholder = "Type claim description"/>
+                </form>
+                <div className="form_buttons_incoming">
+                        <Link to='/'>
+                            <button className="new_cancel_but">
+                                Cancel
+                            </button>       
+                        </Link>
+                        <form action="" className = "incoming_form">
+                            <input type="submit" value="Done" className = "create_but"/>
+                            <input type="submit" value="Decline" className = "create_but"/>
+                        </form>
+                        
+                    </div>
+            </section>
+        )
+    }
+}
