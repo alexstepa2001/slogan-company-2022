509943d89e6b08e3f8b202d4ac774ce935b9fd89
alexstepa2001 <alexstepa2001@icloud.com>
2022-07-11 12:59:43 +0300

    Доделываю верстку

--- slogan-app/src/components/Burger.jsx
+++ slogan-app/src/components/Burger.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Burger = () => {    
-
     return (
-        <div className="burger">
+        <div className="burger" onClick={() => {
+            document.querySelector('.navigation_wrapper').classList.toggle('isActivate');
+        }}>
             <div className="burger_line"></div>
             <div className="burger_line"></div>
             <div className="burger_line"></div>
